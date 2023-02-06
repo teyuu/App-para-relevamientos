50d8b24b2b34004cd1379b809262a06de3305ee7
teyuu <tellini.matias@gmail.com>
2023-02-06 09:08:39 -0300

    localdb

--- API/src/database/db.js
+++ API/src/database/db.js
@@ -1,12 +1,11 @@
 const { Sequelize } = require('sequelize');
 const dotenv = require('dotenv').config();
 
-
 const sequelize = new Sequelize(
-   process.env.MYSQLDATABASE ,
-   process.env.MYSQLUSER ,
-   process.env.MYSQLPASSWORD,{
-        host: process.env.MYSQLHOST,
+   process.env.DB_DATABASE,
+   process.env.DB_USERNAME,
+   process.env.DB_PASSWORD,{
+        host: process.env.DB_HOST,
         dialect: "mysql"
     }
 );
