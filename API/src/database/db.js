4e1d41a8083a8afb0fa19778de648516e1a476b0
teyuu <tellini.matias@gmail.com>
2023-02-06 13:06:18 -0300

    fix rail

--- API/src/database/db.js
+++ API/src/database/db.js
@@ -1,18 +1,16 @@
 const { Sequelize } = require('sequelize');
-require('dotenv').config();
+const dotenv = require('dotenv').config();
 
 const {
   MYSQLDATABASE, MYSQLHOST, MYSQLPASSWORD, MYSQLPORT, MYSQLUSER
 } = process.env;
 const sequelize = new Sequelize(
-  {
-    username:MYSQLUSER,
-    password:MYSQLPASSWORD,
-    database:MYSQLDATABASE,
-    dialect:'mysql',
-    port:MYSQLPORT,
-    host:MYSQLHOST
-  }
+   process.env.DB_DATABASE,
+   process.env.DB_USERNAME,
+   process.env.DB_PASSWORD,{
+        host: process.env.DB_HOST,
+        dialect: "mysql"
+    }
 );
 
 module.exports = sequelize;
