084c61e161574c2761117e243502d8bbfa713ff8
teyuu <tellini.matias@gmail.com>
2023-02-06 09:00:14 -0300

    added rw db

--- API/src/database/db.js
+++ API/src/database/db.js
@@ -1,13 +1,11 @@
 const { Sequelize } = require('sequelize');
 const dotenv = require('dotenv').config();
+const {
+    MYSQLUSER, MYSQLPASSWORD, MYSQLHOST, MYSQLPORT, MYSQLDATABASE
+  } = process.env;
 
 const sequelize = new Sequelize(
-   process.env.DB_DATABASE,
-   process.env.DB_USERNAME,
-   process.env.DB_PASSWORD,{
-        host: process.env.DB_HOST,
-        dialect: "mysql"
-    }
+   `mysql://${{ MYSQLUSER }}:${{ MYSQLPASSWORD }}@${{ MYSQLHOST }}:${{ MYSQLPORT }}/${{ MYSQLDATABASE }}`
 );
 
 module.exports = sequelize;
