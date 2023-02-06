bdde0f25a04960a714692dd03b151ddfa376087a
teyuu <tellini.matias@gmail.com>
2023-02-06 08:49:27 -0300

    -db changed to railwai db

--- API/src/database/db.js
+++ API/src/database/db.js
@@ -2,10 +2,10 @@ const { Sequelize } = require('sequelize');
 const dotenv = require('dotenv').config();
 
 const sequelize = new Sequelize(
-   process.env.DB_DATABASE,
-   process.env.DB_USERNAME,
-   process.env.DB_PASSWORD,{
-        host: process.env.DB_HOST,
+   process.env.MYSQLDATABASE ,
+   process.env.MYSQLUSER ,
+   process.env.MYSQLPASSWORD,{
+        host: process.env.MYSQLHOST,
         dialect: "mysql"
     }
 );
