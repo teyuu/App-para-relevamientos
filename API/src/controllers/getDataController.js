33c8a12dd2d7d749da0b27cfd8572d332df7b99c
teyuu <tellini.matias@gmail.com>
2023-02-07 18:42:12 -0300

    update back

--- API/src/controllers/getDataController.js
+++ API/src/controllers/getDataController.js
@@ -2,46 +2,63 @@ const User = require('../database/models/User');
 const Place = require('../database/models/Place');
 const Result = require('../database/models/Result');
 
-const getUserResults=  (req, res) => {
-    try {
-        User.findAll({
-            include: [{
-                model: Place,
-                attributes: ["name"]
-            },
-            {
-                model: Result,
-                attributes: ['item_relevar', 'decision', 'observaciones', 'placeId']
-            }],
-            attributes: ["userName"]
-        })
-        .then(users => {
-            res.json(users)
-        })
-        .catch(err => {
-            throw new Error(err)
-        })
-    } catch (error) {
-        console.error(error)
-        res.status(500).json({ error: error.message })
-    }
+const getUserData = (req, res) => {
+	try {
+		User.findAll({
+			include: [{
+				model: Place,
+				attributes: ["name"]
+			},
+			{
+				model: Result,
+				attributes: ['item_relevar', 'decision', 'observaciones', 'placeId']
+			}],
+			attributes: ["userName"]
+		})
+			.then(users => {
+				res.json(users)
+			})
+			.catch(err => {
+				throw new Error(err)
+			})
+	} catch (error) {
+		console.error(error)
+		res.status(500).json({ error: error.message })
+	}
 }
 
 const getAllResults = (req, res) => {
-    try {
-        Result.findAll({
-            include: {
-                model: Place,
-                attributes: ['name']
-            },
-            attributes: ['item_relevar', 'decision', 'observaciones', 'userId', 'imagen']
-        }).then(results => {
-            res.json(results)
-        });
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ message: error.message });
-    }
+
+	const { id } = req.params;
+
+	try {
+		
+		if(!id){
+			Result.findAll({
+				include: {
+					model: Place,
+					attributes: ['name']
+				},
+				attributes: ['item_relevar', 'decision', 'observaciones', 'userId', 'imagen']
+			}).then(results => {
+				res.json(results)
+			});
+		}else{
+			Result.findAll({
+				where: { userId: id},
+				include: {
+					model: Place,
+					attributes: ['id','name']
+				},
+				attributes: ['id','item_relevar', 'decision', 'observaciones', 'userId', 'imagen']
+			}).then(results=>{
+				res.json(results)
+			})
+		}
+	} catch (error) {
+		console.error(error);
+		res.status(500).json({ message: error.message });
+	}
 };
 
-module.exports ={getUserResults, getAllResults}
+module.exports = { getUserData, getAllResults }
